20c1bec2017e7067aadd93731b30782e1f63c89c
vhvkhoa <vhvkhoa@gmail.com>
2019-02-18 00:29:57 +0700

    Fit new feature type.

--- core/beam_decoder.py
+++ core/beam_decoder.py
@@ -18,8 +18,6 @@ class BeamSearchDecoder(object):
     def decode(self, features, tags):
         with torch.no_grad():
             features = features.to(device=self.device)
-            print(type(tags))
-            print(tags.size())
             tags = tags.to(device=self.device)
             features = self.model.batch_norm(features)
             tags_embed = self.model.word_embedding(tags)
